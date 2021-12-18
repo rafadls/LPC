37559331a82a754305668193a4d1ea678da3fc3a
Rafael De La Sotta <rafadelasottavargas@gmail.com>
2021-12-18 18:24:47 -0300

    fin lab 10

--- L10/main.py
+++ L10/main.py
@@ -8,9 +8,9 @@ from tinydb.operations import set
 app = FastAPI(title="Lab 6")
 
 # aquí carguen el modelo guardado (con load de joblib) y
-model = None
+model = load("modelo.joblib")
 # el cliente de base de datos (con tinydb). Usen './db.json' como bbdd.
-db = None
+db = TinyDB("./db.json")
 
 # Nota: En el caso que al guardar en la bbdd les salga una excepción del estilo JSONSerializable
 # conviertan el tipo de dato a uno más sencillo.
@@ -23,29 +23,48 @@ db = None
 
 @app.post("/potabilidad/")
 async def predict_and_save(observation: Dict[str, float]):
-    # implementar 1. aquí
-    pass
+    input_post_list = list(observation.values())
+    Prediction = int(model.predict([input_post_list])[0])
+    hoy = datetime.now()
+    observation["Day"] = hoy.day
+    observation["Month"] = hoy.month
+    observation["Year"] = hoy.year
+    observation['Prediction'] = Prediction
+    id_row = db.insert(observation)
+    return {"potabilidad": Prediction, "id": id_row}
 
 
 @app.get("/potabilidad/")
 async def read_all():
     # implementar 2 aquí.
-    pass
+    return db.all()
+
 
 
 @app.get("/potabilidad_diaria/")
 async def read_by_day(day: int, month: int, year: int):
     # implementar 3 aquí
-    pass
+    Mediciones = Query()
+    return db.search((Mediciones.Day == day) & (Mediciones.Month == month) & (Mediciones.Year == year))
 
 
 @app.put("/potabilidad/")
 async def update_by_day(day: int, month: int, year: int, new_prediction: int):
     # implementar 4 aquí
-    pass
+    Mediciones = Query()
+    try:
+        ids = db.update(set("ph", new_prediction),(Mediciones.Day == day) & (Mediciones.Month == month) & (Mediciones.Year == year))
+        return { "success": True, "updated_elements":ids}
+    except:
+        return { "success": False, "updated_elements": []}
 
 
 @app.delete("/potabilidad/")
 async def delete_by_day(day: int, month: int, year: int):
     # implementar 5 aquí
-    pass
+    Mediciones = Query()
+    try:
+        ids = db.remove((Mediciones.Day == day) & (Mediciones.Month == month) & (Mediciones.Year == year))
+        return { "success": True, "deleted_elements":ids}
+    except:
+        return { "success": False, "deleted_elements": []}
